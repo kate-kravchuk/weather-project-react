ec5304ad52148b013ab451006fe8af497d60e4c8
kate-kravchuk <106491612+kate-kravchuk@users.noreply.github.com>
2022-10-07 18:14:38 +0200

    Added links to github and netlify

--- src/App.js
+++ src/App.js
@@ -2,7 +2,22 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather Project React</div>;
+  return (
+    <div className="App">
+      Weather Project React
+      <footer>
+        This project is coded by Kateryna Kravchuk by guidance of Matt Delac. It
+        is{" "}
+        <a href="https://github.com/kate-kravchuk/weather-project-react">
+          open-sourced on GitHub{" "}
+        </a>
+        and{" "}
+        <a href="https://graceful-daifuku-a3b2f5.netlify.app/">
+          deployed on Netlify
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
