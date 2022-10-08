6011f99765632cd2ed95aa73ab987e5350730bcc
kate-kravchuk <106491612+kate-kravchuk@users.noreply.github.com>
2022-10-08 18:19:37 +0200

    Styled temperature elements
    Styled temperature and units
    Placed weather image

--- src/Weather.js
+++ src/Weather.js
@@ -30,15 +30,22 @@ export default function Weather() {
         <li>Wednesday 7:00</li>
         <li>Partly cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="Partly Cloudy"
-          />
-          6°
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="Partly Cloudy"
+              className="float-left"
+            />
+            <div>
+              <span className="temperature">6</span>
+              <span className="unit">°C</span>
+            </div>
+          </div>
         </div>
         <div className="col-6">
+          <span></span>
           <ul>
             <li>Precipitation: 15%</li>
             <li>Humidity: 15%</li>
