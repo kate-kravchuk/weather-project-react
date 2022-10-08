a58636a9f7e79ca6f0c695059a81b8b9d09b577b
kate-kravchuk <106491612+kate-kravchuk@users.noreply.github.com>
2022-10-08 15:03:01 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      Weather Project React
-      <footer>
-        This project is coded by Kateryna Kravchuk by guidance of Matt Delac. It
-        is{" "}
-        <a href="https://github.com/kate-kravchuk/weather-project-react">
-          open-sourced on GitHub{" "}
-        </a>
-        and{" "}
-        <a href="https://graceful-daifuku-a3b2f5.netlify.app/">
-          deployed on Netlify
-        </a>
-      </footer>
+      <div className="container">
+        Weather Project React
+        <footer>
+          This project is coded by Kateryna Kravchuk by guidance of Matt Delac.
+          It is{" "}
+          <a href="https://github.com/kate-kravchuk/weather-project-react">
+            open-sourced on GitHub{" "}
+          </a>
+          and{" "}
+          <a href="https://graceful-daifuku-a3b2f5.netlify.app/">
+            deployed on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
