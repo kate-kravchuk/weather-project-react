ae48da26556fa0dc068c3e8857b163fe785a446e
kate-kravchuk <106491612+kate-kravchuk@users.noreply.github.com>
2022-10-08 15:41:28 +0200

    Added a html in Weather.js
    Created the html structure of the future app

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        Weather Project React
         <Weather />
         <footer>
           This project is coded by Kateryna Kravchuk by guidance of Matt Delac.
--- src/Weather.js
+++ src/Weather.js
@@ -2,5 +2,44 @@ import React from "react";
 import "./Weather.css";
 
 export default function Weather() {
-  return <div className="Weather">Hello from Weather!!!!</div>;
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="enter a city"
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+
+      <h1>New York</h1>
+      <ul>
+        <li>Wednesday 7:00</li>
+        <li>Partly cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="Partly Cloudy"
+          />
+          6°
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: 15%</li>
+            <li>Wind: 2%</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
