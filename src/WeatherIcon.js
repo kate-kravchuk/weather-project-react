69ee2bc17e7ee27cf17afd15ff73b5b7837f9469
kate-kravchuk <106491612+kate-kravchuk@users.noreply.github.com>
2022-10-12 15:45:59 +0200

    Added a dexcription icon from site
    I take custom weather animated icons from site: npjs.com/package/react-animated-weather

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -3,9 +3,11 @@ import ReactAnimatedWeather from "react-animated-weather";
 
 export default function WeatherIcon(props) {
   return (
-    <img
-      src="https://raw.githubusercontent.com/divyanshu013/react-animated-weather/HEAD/react-animated-weather.gif"
-      alt={props.alt}
+    <ReactAnimatedWeather
+      icon="CLEAR_DAY"
+      color="orange"
+      size={48}
+      animate={true}
     />
   );
 }
