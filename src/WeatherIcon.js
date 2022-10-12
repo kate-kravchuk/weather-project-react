4e90120a509ab2e0437c29f70f0a3aeaa277dd92
kate-kravchuk <106491612+kate-kravchuk@users.noreply.github.com>
2022-10-12 15:32:24 +0200

    Created a structure for the icon

---
+++ src/WeatherIcon.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactAnimatedWeather from "react-animated-weather";
+
+export default function WeatherIcon(props) {
+  return (
+    <img
+      src="https://raw.githubusercontent.com/divyanshu013/react-animated-weather/HEAD/react-animated-weather.gif"
+      alt={props.alt}
+    />
+  );
+}
