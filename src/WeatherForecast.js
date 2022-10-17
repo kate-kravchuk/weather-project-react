1ef2220a6b27c0e12e83fc889383ab51c700f868
kate-kravchuk <106491612+kate-kravchuk@users.noreply.github.com>
2022-10-17 15:28:09 +0200

    Deleted logo

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,18 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
 export default function WeatherForecast() {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+  let apiKey = "515c9ddbeb3cda9061acfab71031839e";
+  let longitude = 36.2;
+  let latitude = 49.9;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&${longitude}&appid=${apiKey}`;
+  axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="WeatherForecast">
       <div className="row">
