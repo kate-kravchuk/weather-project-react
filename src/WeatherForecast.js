005078439735d572d53a822bb3e8e002d81e5597
kate-kravchuk <106491612+kate-kravchuk@users.noreply.github.com>
2022-10-18 22:31:32 +0200

    Experiments with WeatherForecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -23,11 +23,13 @@ export default function WeatherForecast(props) {
       </div>
     );
   } else {
-    let apiKey = "515c9ddbeb3cda9061acfab71031839e";
+    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
     let longitude = props.coordinates.lon;
-    let latitude = 49.9;
+    let latitude = props.coordinates.lat;
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
     axios.get(apiUrl).then(handleResponse);
+
     return null;
   }
 }
