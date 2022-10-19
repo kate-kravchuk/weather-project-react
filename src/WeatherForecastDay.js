9e83672e9177aa2b30aad92a6ef7ef2d08f0058d
kate-kravchuk <106491612+kate-kravchuk@users.noreply.github.com>
2022-10-19 14:34:21 +0200

    Added a use Effect
    Added a use Effect for the changing of forecast every time the city is changing

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -22,6 +22,7 @@ export default function WeatherForecastDay(props) {
   return (
     <div>
       <div className="WeatherForecast-day">{day()}</div>
+
       <WeatherIcon code={props.data.weather[0].icon} size={22} />
       <div className="WeatherForecast-temperatures">
         <span className="WeatherForecast-temperatures-max">
